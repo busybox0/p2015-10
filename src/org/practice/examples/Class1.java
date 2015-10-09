5e7146fb99077931debc7b4f96b0bfe701e36071
Busy Box0 <busybox0@gmail.com>
2015-10-09 10:21:15 +0300

    One

---
+++ src/org/practice/examples/Class1.java
@@ -0,0 +1,17 @@
+package org.practice.examples;
+
+/**
+ * Created by max on 2015-10-08.
+ */
+public class Class1 {
+    String s1 = new String("Hello Max!");
+    String s2 = new String("");
+    String s3;
+
+    void show() {
+        System.out.println(s1.isEmpty() ? "isEmpty" : "Not empty");
+
+    }
+
+
+}
---
+++ src/org/practice/examples/Main.java
@@ -0,0 +1,11 @@
+package org.practice.examples;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Class1 c1 = new Class1();
+        c1.show();
+    }
+}
